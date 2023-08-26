8ae7ea73320aee4c2395da3b064e3ccee2968bde
YungButtCoin <yungz0rk@gmail.com>
2023-08-26 18:15:26 -0500

    Most base functionality complete; needs itemized income

--- rental_income.py
+++ rental_income.py
@@ -6,17 +6,8 @@
 # the Return on Investment(ROI) for a rental property. And now that we know a thing or two about making our programs 
 # run more efficiently, try utilizing some of the strategies we talked about this week for optimization!
 
-# Your Program should have the following (this is not an exhaustive list but just base functionality):
 
-# -  There should be some sort of Driver code for users to choose what to do next 
-
-# -  There should be a way to store multiple users and for users to be able to have multiple different properties 
-#    (This might be done by creating a User Class & a Property class similarly to how we did it with the Codeflix program)
-
-# -  Properties should be able to store multiple different types of expenses (i.e. taxes, mortgage, insurance, etc) and 
-#    multiple different types of incomes (i.e. rent, laundry, parking, etc)
-
-# -  The ROI needs to be calculated & displayed to the user and should also be stored for that specific property.
+# -  Properties should be able to store multiple different types of incomes (i.e. rent, laundry, parking, etc)
 
 
 
@@ -24,37 +15,119 @@
 
 
 
-# Still to do: flesh out User class, create UserDatabase class, create general flow of things, update Property class to have multiple forms of income instead of just 1 lump sum
+# Still to do: update Property class to have multiple forms of income instead of just 1 lump sum, as well as being able to adjust attributes after initial setup
 
+class UserDatabase():
+
+    def __init__(self):
+        self.users = {}
+        self.current_user = None
+    
+    def add_user(self):
+        name = input('What is going to be your username? ').lower()
+        if name in self.users:
+            print(f'The username {name} has already been taken')
+        else:
+            pw = input(f'What is your password for {name}? ')
+            self.users[name] = User(name,pw)
+            print(f'User {name} has been created with password {pw}')
+
+    def delete_user(self):
+        if self.current_user:
+            choice = input(f"You would like to delete {self.current_user.name}, correct? (y/n) ").lower()
+            if choice in {'y', 'ya', 'yes', 'yeah', 'correct'}:
+                pw = input(f'In order to delete, please enter the password for {self.current_user.name} ')
+                if self.current_user.check_password(pw):
+                    print(f'{self.current_user.name} has been deleted from the system and you have been logged out')
+                    name = self.current_user.name.lower()
+                    self.current_user = None
+                    self.users.pop(name)
+                else:
+                    print('Password is not valid, unable to delete')
+                    self.logout()
+            else:
+                print(f'{self.current_user.name} was not deleted')
+        else:
+            name = input(f"What is the name of the user you would like to delete? ").lower()
+            if name in self.users:
+                pw = input(f'In order to delete, please enter the password for {name} ')
+                if self.users[name].check_password(pw):
+                    print(f'{name} has been deleted from the system')
+                    self.users.pop(name)
+                else:
+                    print('Password is not valid, unable to delete')
+            else:
+                print(f'{name} is not a valid registered user')
+
+    def login(self):
+        if self.current_user:
+            choice = input(f'{self.current_user.name} is already logged in.  Would you like to log them out?  (y/n) ').lower()
+            if choice in {'y', 'ya', 'yes', 'yeah'}:
+                self.logout()
+            else:
+                print(f'{self.current_user.name} will stay logged in')
+        else:
+            name = input('Please enter your username: ').lower()
+            pw = input('Please enter your password: ').lower()
+            if name in self.users and self.users[name].check_password(pw):
+                print(f'Welcome back, {name}')
+                self.current_user = self.users[name]
+            else:
+                print("Invalid user name and password combo")
+    
+    def logout(self):
+        if self.current_user:
+            print(f'{self.current_user.name} has been successfully logged out')
+            self.current_user = None
+        else:
+            print('There is no user currently logged in')
 
 class User():
 
     id_counter = 1
+
     def __init__(self, name, password):
-        self.name = name.lower()
+        self.name = name.title()
         self.password = password
         self.id = User.id_counter
         User.id_counter += 1
         self.properties = {}
 
-    def check_password(self, password_guess):
-        return self.password == password_guess[::-2]
+    def __repr__(self):
+        return f"<User {self.id} | {self.name}>"
     
-    def add_property(self, property):
-        self.properties[len(self.properties) +1] = Property()
+    def check_password(self, password_guess):
+        return self.password[::-2] == password_guess[::-2]
+    
+    def add_property(self):
+        prop_name = input('What is the name of this property? ').title()
+        nameset = {value.name for value in self.properties.values()}
+        if prop_name in nameset:
+            print(f'You have already logged {prop_name}')
+        else:
+            self.properties[str(len(self.properties) +1)] = Property(prop_name)
+            self.show_properties()
 
     def remove_property(self):
-        pass
+        for property in self.properties.items():
+            print(property)
+        deletion = input("Which property do you want to delete?  Type the number to delete it: ")
+        if deletion in self.properties:
+            print(f'{self.properties[deletion].name} has been deleted')
+            self.properties.pop(deletion)
+            self.show_properties()
+        else:
+            print(f'{deletion} is not a valid selection')
 
     def show_properties(self):
-        print(f'You currently have {len(self.properties)}:')
-        for property in self.properties:
+        print('You have logged the following properties:')
+        for property in self.properties.items():
             print(property)
 
 class Property():
 
-    def __init__(self):
-        self.name = ""
+    def __init__(self, name):
+        self.name = name
         self.address = ""
         self.income = 0
         self.individual_expenses = {}
@@ -68,6 +141,9 @@ class Property():
         self.roi = 0
         self.data = {}
         self.generate_info()
+
+    def __repr__(self):
+        return f'{self.name.title()}: makes ${self.income} per month; ROI: {self.roi}%'
 
     def property_data(self):
         self.data['name'] = self.name
@@ -89,7 +165,6 @@ class Property():
     # May just want to have the user also generate name and address as well    
     def generate_info(self):
 
-        self.name = input('What is the name of this property? ').title()
         self.address = input(f'What is the address of {self.name}? ')
 
         # Getting the monthly income
@@ -126,7 +201,7 @@ class Property():
 
     # Updates the expenses dictionary, as well as totaling up the expenses while doing so
     def update_expenses(self):
-        add_expenses = input(f'Would you like to add any expenses for {self.name}? (y/n) ').lower()
+        add_expenses = input(f'Would you like to add any monthly expenses for {self.name}? (y/n) ').lower()
         if add_expenses in {'y', 'yes', 'yeah', 'ya', 'you bet your ass i do'}:
             total = self.total_expenses
             while True:    
@@ -136,7 +211,7 @@ class Property():
                     if expense_cost >= 0 and expense_type not in self.individual_expenses:
                         self.individual_expenses[expense_type] = expense_cost
                         total += expense_cost
-                        add_another = input(f'Your list of expenses for {self.name} so far are \n {self.individual_expenses} \n and are ${total} in total. \
+                        add_another = input(f'Your list of monthly expenses for {self.name} so far are \n {self.individual_expenses} \n and are ${total} in total. \
                                             \nWould you like to add another expense for {self.name}? (y/n) ').lower()
                         if add_another not in {'y', 'yes', 'yeah', 'ya', 'you bet your ass i do'}:
                             print(f'Your list of expenses for {self.name} are \n {self.individual_expenses} \n and are ${total} in total.')
@@ -144,15 +219,15 @@ class Property():
                             break   
                     else:
                         if expense_type in self.individual_expenses:
-                            change_cost = input(f'{expense_type} is already in your list of expenses as ${self.individual_expenses[expense_type]}.  Do you want to update the cost to be ${expense_cost}? (y/n) ').lower()
+                            change_cost = input(f'{expense_type} is already in your list of monthly expenses as ${self.individual_expenses[expense_type]}.  Do you want to update the cost to be ${expense_cost}? (y/n) ').lower()
                             if change_cost in {'y', 'yes', 'yeah', 'ya', 'you bet your ass i do'}:
                                 total -= self.individual_expenses[expense_type]
                                 self.individual_expenses[expense_type] = expense_cost
                                 total += expense_cost
-                            add_another = input(f'Your list of expenses for {self.name} so far are \n {self.individual_expenses} \n and are ${total} in total. \
+                            add_another = input(f'Your list of monthly expenses for {self.name} so far are \n {self.individual_expenses} \n and are ${total} in total. \
                                         \nWould you like to add another expense for {self.name}? (y/n) ').lower()
                             if add_another not in {'y', 'yes', 'yeah', 'ya', 'you bet your ass i do'}:
-                                print(f'Your list of expenses for {self.name} are \n {self.individual_expenses} \n and are ${total} in total.')
+                                print(f'Your list of monthly expenses for {self.name} are \n {self.individual_expenses} \n and are ${total} in total.')
                                 self.total_expenses = total
                                 break 
                         else:
@@ -200,4 +275,37 @@ class Property():
         self.roi = 100*((12*self.c_flow)/self.initial_investment)
         print(f"{self.name}'s cash on cash ROI is currently {self.roi}%")
         
-my_property = Property()
+print('Welcome to rental property calculator!')
+users = UserDatabase()
+while True:
+    if users.current_user:
+        choice = input("What would you like to do?  Delete Account (D), Login (L), Logout (O), Add Property (A), Remove Property (R), Show Properties (S), Quit (Q): ").lower()
+        if choice in {'d', 'del', 'delete', 'delete account'}:
+            users.delete_user()
+        elif choice in {'l', 'login', 'log in'}:
+            users.login()
+        elif choice in {'o', 'logout', 'log out'}:
+            users.logout()
+        elif choice in {'a', 'add', 'add property'}:
+            users.current_user.add_property()
+        elif choice in {'r', 'remove', 'remove property'}:
+            users.current_user.remove_property()
+        elif choice in {'s', 'show', 'show properties'}:
+            users.current_user.show_properties()
+        elif choice in {'q', 'quit', 'exit'}:
+            break
+        else:
+            print("Invalid selection")
+    else:
+        choice = input("What would you like to do?  Register (R), Delete Account (D), Login (L), Quit (Q): ").lower()
+        if choice in {'r', 'reg', 'register'}:
+            users.add_user()
+        elif choice in {'d', 'del', 'delete', 'delete account'}:
+            users.delete_user()
+        elif choice in {'l', 'login', 'log in'}:
+            users.login()
+        elif choice in {'q', 'quit', 'exit'}:
+            break
+        else:
+            print("Invalid selection")
+print('Thank you for using rental property calculator!')
